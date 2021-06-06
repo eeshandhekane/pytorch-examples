2d3658c4db38a8b3f4c7a52d3ecef4668c7729fc
eeshandhekane <eeshandhekane@gmail.com>
2021-06-06 14:34:13 +0530

    example_1.ipynb: added; .gitignore: updated with venv

---
+++ __init__.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env/python3
+
+
+"""
+About
+-----
+
+Classes
+-------
+
+Functions
+---------
+
+Variables
+---------
+"""
+
+# Meta-data.
+__author__ = 'Eeshan Gunesh Dhekane'
+__copyright__ = ''
+__credits__ = []
+__license__ = ''
+__version__ = '1.0'
+__maintainer__ = 'Eeshan Gunesh Dhekane'
+__email__ = 'eeshandhekane@gmail.com'
+__status__ = ''
+
+# Dependencies.
+
+
+# Main.
+if __name__ == '__main__':
+	pass
